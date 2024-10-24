7b16dba7ab2f6ae73411f05582b118c11191603a
Andy Hall (fenomas) <fenomas@mercari.com>
2024-10-24 15:18:27 +0900

    fix build links

--- src/music/music.ts
+++ src/music/music.ts
@@ -4,6 +4,7 @@ import { setPathStr } from './Main'
 import { midiToFreq } from './util'
 
 const rand = (a = 0, b = 1) => (a + Math.random() * (b - a + 1)) | 0
+const abs = (n: number) => Math.abs(n)
 let ct = 0
 const major = [0, 2, 4, 5, 7, 9, 11]
 const makeChord = (n = 0) => [n, n + 2, n + 4].map((n) => major[n % 7])
@@ -25,6 +26,26 @@ export const playChords = (gen: any) => {
   ct = (ct + 1) % 4
 }
 
+// helpers to make the demo simpler
+const chooseNoteTarget = (note: number) => {
+  let tgt = note + rand(-6, 6)
+  if (tgt === note) tgt++
+  if (!inMajorScale(tgt)) tgt++
+  if (Math.abs(tgt - note) > 8) tgt += tgt > note ? -12 : 12
+  return tgt
+}
+const chooseNoteDur = (notes: number[]) => {
+  let noteDur = beatDur * 2
+  if (notes.length > 2) noteDur /= 2
+  if (notes.length > 4) noteDur /= 2
+  return noteDur
+}
+const tendTowardsCenter = (note: number) => {
+  note = ((72 + note) / 2) | 0
+  if (!inMajorScale(note)) note++
+  return note
+}
+
 /**
  *
  *
@@ -33,29 +54,23 @@ export const playChords = (gen: any) => {
  */
 
 const finder = new Pathfinder<number>({
-  getNeighbors: (n) => [0, 0, 0, 0].map(() => n + rand(-3, 3)),
-  getHeuristic: (a, b) => 5 + Math.abs(a - b) + rand(0, 3),
-  getMoveCost: (a, b) => Math.abs(a - b) + rand(1, 2) + (inMajorScale(b) ? 1 : 2),
+  getNeighbors: (n) => [0, 0, 0, 0, 0, 0].map(() => n + rand(-3, 3)),
+  getHeuristic: (a, b) => 5 + abs(a - b) + rand(0, 3),
+  getMoveCost: (a, b) => abs(a - b) + rand(1, 4) + (inMajorScale(b) ? 1 : 5),
 })
-console.log('finder')
+
 let note = 72
 export const playNotes = (gen: any) => {
-  let tgt = note + rand(-6, 6)
-  if (tgt === note) tgt++
-  if (!inMajorScale(tgt)) tgt++
-  if (Math.abs(tgt - note) > 8) tgt += tgt > note ? -12 : 12
-
+  const tgt = chooseNoteTarget(note)
   const path = finder.findPath(note, tgt)
-  setPathStr(`[${path.join(', ')}]`)
 
-  let noteDur = beatDur * 2
-  if (path.length > 2) noteDur /= 2
-  if (path.length > 4) noteDur /= 2
   let t = gen.now() + 0.05
+  const noteDur = chooseNoteDur(path)
   path.forEach((note, i) => {
     gen.play(Piano2, midiToFreq(note), vel, t, t + noteDur)
     t += noteDur
   })
-  note = ((72 + tgt) / 2) | 0
-  if (!inMajorScale(note)) note++
+
+  note = tendTowardsCenter(tgt)
+  setPathStr(`[${path.join(', ')}]`)
 }
